aaef4b8605ad5b274dbcf1c4476580c821c48588
Marc Baldwin <marc.baldwin88@gmail.com>
2020-12-09 00:38:09 +0000

    Update Retrofit to 2.6.2

--- strava/src/main/java/xyz/marcb/strava/error/StravaErrorAdapter.kt
+++ strava/src/main/java/xyz/marcb/strava/error/StravaErrorAdapter.kt
@@ -7,7 +7,7 @@ import retrofit2.HttpException
 object StravaErrorAdapter {
 
     private val responseAdapter: JsonAdapter<StravaErrorResponse> =
-            Moshi.Builder().build().adapter(StravaErrorResponse::class.java)
+        Moshi.Builder().build().adapter(StravaErrorResponse::class.java)
 
     fun convert(error: Throwable): Throwable {
         return when (error) {
@@ -17,20 +17,23 @@ object StravaErrorAdapter {
     }
 
     private fun convertHttpException(error: HttpException): Throwable {
-        val response = error.response().errorBody().string()
-        val errorResponse = responseAdapter.fromJson(response)
-        return convert(errorResponse, error)
+        val response = error.response()?.errorBody()?.string() ?: return error
+        try {
+            val errorResponse = responseAdapter.fromJson(response) ?: return error
+            return convert(errorResponse, error)
+        } catch (jsonDecodingError: Throwable) {
+            return error
+        }
     }
 
     private fun convert(response: StravaErrorResponse, error: HttpException): Error {
         return when (error.code()) {
             400 -> {
-                val isNotAuthorized = response.errors?.firstOrNull { it.resource == StravaErrorResponse.refreshToken } != null
-                if (isNotAuthorized) {
-                    StravaError.RefreshTokenInvalid
-                } else {
-                    StravaError.BadRequest(response)
-                }
+                val isNotAuthorized = response.errors?.firstOrNull {
+                    it.resource == StravaErrorResponse.refreshToken
+                } != null
+                if (isNotAuthorized) StravaError.RefreshTokenInvalid
+                else StravaError.BadRequest(response)
             }
             401 -> StravaError.AccessTokenInvalid
             403 -> StravaError.Forbidden(response)

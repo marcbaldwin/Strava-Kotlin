5522f6f9195c0a734833b7dd1ccd070456fe513d
Marc Baldwin <marc.baldwin88@gmail.com>
2024-08-08 15:56:44 +0100

    Make Activity.id a Long

--- strava/src/main/java/xyz/marcb/strava/Activity.kt
+++ strava/src/main/java/xyz/marcb/strava/Activity.kt
@@ -4,7 +4,7 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class Activity(
-    val id: String,
+    val id: Long,
     val name: String,
     val description: String?,
     val distance: Float,
